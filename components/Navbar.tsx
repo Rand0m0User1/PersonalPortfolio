2778e7f8335ac042036141ae8574f6faa9b9e201
Aleksander Kurgan <aleksanderkurgan9@gmail.com>
2024-07-26 23:48:07 -0400

    added menu buttton for mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,28 +8,28 @@ import { isLabeledStatement } from "typescript"
 import React, { useEffect, useState } from "react"
 
 interface NavItem {
-    label: string
-    page: string
-  }
+  label: string
+  page: string
+}
 
-  const NAV_ITEMS: Array<NavItem> = [
-    {
-      label: "Home",
-      page: "home",
-    },
-    {
-      label: "About",
-      page: "about",
-    },
-    {
-      label: "Projects",
-      page: "projects",
-    },
-    {
-      label: "Résumé",
-      page: "resume",
-    },
-  ]
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
+  {
+    label: "Résumé",
+    page: "resume",
+  },
+]
 
 const Navbar = () => {
   const pathname = usePathname()
@@ -37,42 +37,51 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false)
   const [themeLoaded, setThemeLoaded] = useState(false)
 
-    useEffect(() => setThemeLoaded(true), [theme])
+  useEffect(() => setThemeLoaded(true), [theme])
 
-    if (!themeLoaded) return null
+  if (!themeLoaded) return null
 
-    return (
-      <header className={`w-full mx-auto px-4 fixed top-0 z-50 ${resolvedTheme === "dark" ? "bg-stone-900 dark:border-b dark:border-stone-600" : "bg-white shadow"}`}>
-        <div className="justify-between md:items-center md:flex">
-          <div>
+  return (
+    <header className={`w-full mx-auto px-4 fixed top-0 z-50 ${resolvedTheme === "dark" ? "bg-stone-900 dark:border-b dark:border-stone-600" : "bg-white shadow"}`}>
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex justify-between items-center py-3">
             <div className="md:py-5 md:block">
               <h2 className="text-2xl font-bold">Aleksander Kurgan</h2>
             </div>
+            <div className="md:hidden">
+              <button className="p-2">
+                {navbar ? <IoMdClose size={30}/> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
+        </div>
+        <div className={`flex justify-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"}`}>
           <div className="items-center justify-center md:flex md:space-x-6">
             {NAV_ITEMS.map((item, idx) => {
               return <a key={idx}>{item.label}</a>  
             })}
             
             {resolvedTheme === "dark" ? (
-                <button
-                  onClick={() => setTheme("light")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
-                  <RiSunLine size={25} color="black" />
-                </button>
-              ) : (
-                <button
-                  onClick={() => setTheme("dark")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
-                  <RiMoonFill size={25} />
-                </button>
-              )}
+              <button
+                onClick={() => setTheme("light")}
+                className="bg-slate-100 p-2 rounded-xl"
+              >
+                <RiSunLine size={25} color="black" />
+              </button>
+            ) : (
+              <button
+                onClick={() => setTheme("dark")}
+                className="bg-slate-100 p-2 rounded-xl"
+              >
+                <RiMoonFill size={25} />
+              </button>
+            )}
           </div>
         </div>
-      </header>
-    );
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
