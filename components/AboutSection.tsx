c49f02598e5b0798e5a1156266013157663039ca
Aleksander Kurgan <aleksanderkurgan9@gmail.com>
2024-07-28 12:32:27 -0400

    finished about & skills

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -3,31 +3,30 @@ import React from 'react'
 import Image from 'next/image'
 
 const skills = [
-    { skill: "Python", img: "/python.png"},
-    { skill: "HTML" },
-    { skill: "CSS" },
-    { skill: "Javascript" },
-    { skill: "Next.js" },
-    { skill: "Tailwind CSS" },
-    { skill: "Flutter" },
-    { skill: "Git" },
-    { skill: "Computer Aided Design (CAD)" },
-    { skill: "Computer Aided Manufacturing (CAM)" },
-    { skill: "Onshape (CAD)" },
-    { skill: "Fusion 360 (CAM)" },
-    { skill: "Fabrication" },
-    { skill: "Music Performance" },
-    { skill: "Solo Performance" },
-    { skill: "Big Band" },
-    { skill: "Jazz Improvisation" },
-    { skill: "Agile Methodologies (SCRUM)" },
+    { skill: "Python", img: "/python.svg"},
+    { skill: "HTML", img: "/html.svg" },
+    { skill: "CSS", img: "/css.svg" },
+    { skill: "Javascript", img: "/js.svg" },
+    { skill: "Next.js", img: "/next.svg" },
+    { skill: "Tailwind CSS", img: "/tailwind.svg" },
+    { skill: "Flutter", img: "/flutter.svg" },
+    { skill: "Git", img: "/git.svg"  },
+    { skill: "Onshape (CAD)", img: "/onshape.png"  },
+    { skill: "Fusion 360 (CAM)", img: "/fusion.png"  },
+    { skill: "Music Performance", img: "/music.svg"  },
+    { skill: "Solo Performance", img: "/euph.webp" },
+    { skill: "Big Band", img: "/bigband.png" },
+    { skill: "Jazz Improvisation", img: "/trombone.svg" },
+    { skill: "Agile Methodologies (SCRUM)", img: "/scrum.png" },
   ]
 
   const AboutSection = () => {
     return (
         <section id="about">
             <div className="my-12 pb-12 md:pt-16 md:pb-48">
-                <h1 className="text-center font-bold text-4xl py-8">About Me</h1>
+                <h1 className="text-center font-bold text-4xl py-8">About Me
+                    <hr className="w-6 h-1 mx-auto my-4 bg-amber-200 border-0 rounded"></hr>
+                </h1>
                 <div className="flex flex-col space-y-5 items-stretch justify-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10">
                     <div className="text-center md:text-left md:w-1/2">
                         <p>FillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFillerFiller</p>
@@ -40,14 +39,14 @@ const skills = [
                         </div>
                     </div>
                 </div>
-                <h1 className="text-center font-bold text-2xl mb-6">My Current Skillset</h1>
+                <h1 className="text-center font-bold text-2xl mb-6 mt-20">My Current Skillset</h1>
                 <div className="flex justify-center">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+                    <div className="flex flex-wrap justify-center gap-6 lg:gap-10">
                         {skills.map((item, idx) => {
                             return (
-                                <div key={idx} className="w-40 h-40 flex flex-col items-center justify-center bg-gray-200 p-4 rounded-lg shadow-lg">
-                                    <div className="w-16 h-16 mb-4">
-                                        <Image src={item.img} alt={item.skill} width={64} height={64} unoptimized className="object-contain" />
+                                <div key={idx} className="w-40 h-40 flex flex-col items-center justify-center bg-gray-200 p-4 rounded-lg shadow-lg border-4 border-amber-500 hover:bg-gray-300">
+                                    <div className="w-16 h-16 relative mb-2">
+                                        <Image src={item.img} alt={item.skill} layout="fill" objectFit="contain" />
                                     </div>
                                     <p className="text-gray-500 font-semibold text-center">{item.skill}</p>
                                 </div>
